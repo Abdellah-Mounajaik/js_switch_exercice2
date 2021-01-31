340aa5f03be355a6f49b081ff93841ad24aff614
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 01:20:10 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,69 @@
+// Exo1
+
+// let nbr = parseInt(prompt('Entrez un nombre:'));
+
+//  console.log(nbr % 2);
+//  console.log(nbr % 2 == 0);
+
+//  switch (isNaN(nbr)) {
+//     case true:
+//         console.log('Error -> NaN');
+//         break;
+//     case false:
+//         switch (nbr % 2 == 0) {
+//             case true:
+//                 console.log(`${nbr} est un nombre paire`);
+//                 break;
+//             case false:
+//                 console.log(`${nbr} est un nombre impaire`);
+//                 break;
+//             }
+//         break;
+// }
+
+
+
+// exo2
+
+// let mois = prompt("entrez le mois")
+// switch (mois) {
+//     case "1" :
+//         alert("vous êtes en janvier")
+//         break;
+//     case "2" :
+//         alert("vous êtes en fevrier")
+//         break;
+//     case "3" :
+//         alert("vous êtes en mars")
+//         break;
+//     case "4" :
+//         alert("vous êtes en avril")
+//         break;
+//     case "5" :
+//         alert("vous êtes en mai")
+//         break;
+//     case "6" :
+//         alert("vous êtes en juin")
+//         break; 
+//     case "7" :
+//         alert("vous êtes en juillet")
+//         break;
+//     case "8" :
+//         alert("vous êtes en aout")
+//         break;
+//     case "9" :
+//         alert("vous êtes en septembre")
+//         break;
+//     case "10" :
+//         alert("vous êtes en octobre")
+//         break;
+//     case "11" :
+//         alert("vous êtes en novembre")
+//         break;
+//     case "12" :
+//         alert("vous êtes en decembre")
+//         break;       
+//     default:
+//         alert("mois inconnue")
+//         break;
+// }
